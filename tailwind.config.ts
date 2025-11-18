02a4f707e13c691368f248e54de906fd54ba91d9
ABoldAgent <126523318+thespiritofecsta@users.noreply.github.com>
2025-11-18 17:54:56 +0100

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
